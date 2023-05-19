689beb161a7378580a7bab31b458fe70de652a3c
Preisler <preisler.andras@gmail.com>
2023-05-19 23:29:43 +0200

    main

--- comp/button.py
+++ comp/button.py
@@ -2,24 +2,28 @@ import pygame
 
 
 class Button:
-    def __init__(self, x, y, width, height, text, text_color, font, bg_color):
+    def __init__(self, x, y, width, height):
         self.surface = pygame.Surface((width, height))
-        self.surface.fill(bg_color)
         self.rect = self.surface.get_rect(topleft=(x, y))
-        self.text = text
-        self.text_color = text_color
-        self.font = font
-        self.bg_color = bg_color
-        self.text_surface = self.font.render(self.text, True, self.text_color)
-        self.text_rect = self.text_surface.get_rect(center=self.rect.center)
 
     def draw(self, screen):
         screen.blit(self.surface, self.rect)
-        screen.blit(self.text_surface, self.text_rect)
 
-    def update(self, event):
-        if event.type == pygame.MOUSEBUTTONDOWN:
+
+class MapButton(Button):
+    def __init__(self, x, y, width, height):
+        super().__init__(x, y, width, height)
+        self.color = (0, 0, 0)
+        self.was_set = False
+
+    def draw(self, screen):
+        if self.was_set:
+            self.surface.fill(self.color)
+        screen.blit(self.surface, self.rect)
+
+    def update(self, event, player):
+        if self.was_set:
+            return
+        if event.type == pygame.MOUSEBUTTONDOWN and event.button == 1:
             if self.rect.collidepoint(event.pos):
-                print("Button clicked")
-                return True
-        return False
+                self.color = player.color
--- util/round.py
+++ util/round.py
@@ -1,9 +1,19 @@
+from comp.button import MapButton
+import math
+from appConst import screen
+
+
 class GameRound:
     def __init__(self, player1, player2, game_round, active_player):
         self.player1 = player1
         self.player2 = player2
         self.game_round = game_round
         self.active_player = active_player
+        self.map = [MapButton((i % 3) * (screen.width/3), math.ceil(i/3) * (screen.height/3), screen.width/3, screen.height/3)
+                    for i in range(9)]
+
+    def setingSquer(self, squer, player):
+        self.map[squer] = player.color
 
     def chActivePlayer(self):
         if self.active_player == self.player1:
